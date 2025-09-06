7fcdd7e9ef86bbe180a43d14717cdd43b1a51110
Laurynas <laurynas.baltrusaitis@adaptfy.com>
2025-09-06 09:18:12 +0200

    Refactored preprocessing function to be more modular

--- src/data_loading/preprocessing/preprocessing.py
+++ src/data_loading/preprocessing/preprocessing.py
@@ -9,31 +9,41 @@ from .cadastral_info import (
 )
 from .neighborhood_statistics import expand_neighborhood_details_column
 from .postal_city_details import preprocess_postal_city_column
-from .utils import parse_price, parse_size, parse_year, coerce_numeric
+from .utils import (
+    parse_price,
+    parse_size,
+    parse_year,
+    coerce_numeric,
+    apply_parsers,
+)
 
 
 def preprocess_df(df, drop_raw=False, numeric_cols=None):
-    df["price_num"] = df["price"].apply(parse_price)
-    df["contribution_vve_num"] = df["contribution"].apply(parse_price)
-    df["size_num"] = df["size"].apply(parse_size)
-    df["external_storage_num"] = df["external_storage"].apply(parse_size)
+    df = apply_parsers(
+        df,
+        {
+            "price": parse_price,
+            "contribution": parse_price,
+            "size": parse_size,
+            "external_storage": parse_size,
+            "year_of_construction": parse_year,
+        },
+    )
 
     if numeric_cols:
-        df[numeric_cols] = coerce_numeric(df, numeric_cols)
+        df[numeric_cols] = df[numeric_cols].apply(coerce_numeric)
 
-    df = preprocess_outdoor_features_column(df)
-
-    df = preprocess_facilities_column(df)
-
-    df = preprocess_cadastral_column(df)
-    df = preprocess_ownership_column(df)
-    df = preprocess_charges_column(df)
-
-    df = preprocess_postal_city_column(df)
-
-    df = expand_neighborhood_details_column(df)
-
-    df["year_of_construction"] = df["year_of_construction"].apply(parse_year)
+    preprocessing_steps = [
+        preprocess_outdoor_features_column,
+        preprocess_facilities_column,
+        preprocess_cadastral_column,
+        preprocess_ownership_column,
+        preprocess_charges_column,
+        preprocess_postal_city_column,
+        expand_neighborhood_details_column,
+    ]
+    for step in preprocessing_steps:
+        df = step(df)
 
     if drop_raw:
         columns_to_drop = [
